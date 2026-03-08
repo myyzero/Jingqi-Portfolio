e58f9475d46a2d1ac11d6141c59e95a18900179f
myyzero <1912291851@qq.com>
2026-03-08 10:28:24 +0000

    change title from ...Prompt to Jingqi Portfolio

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,14 @@ import { InteractiveProjects } from "./components/InteractiveProjects";
 import { VisualPractice } from "./components/VisualPractice";
 import { Contact } from "./components/Contact";
 import { Navigation } from "./components/Navigation";
+import { useEffect } from "react";
 
 export default function App() {
+  // 设置浏览器标签页标题
+  useEffect(() => {
+    document.title = "Jingqi Portfolio";
+  }, []);
+
   return (
     <div className="min-h-screen bg-white">
       <Navigation />
